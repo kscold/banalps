4de1a6f55b693c18b22a4b8627410031d1b22904
heeyoung123 <heeyoung321@naver.com>
2025-09-24 20:10:09 +0900

    재수술과 흉터이식 / 3번 영역 서브텍스트 줄바꿈이 어색하지 않도록 Word break 적용

--- src/app/hair-transplant/reoperation/page.tsx
+++ src/app/hair-transplant/reoperation/page.tsx
@@ -174,6 +174,36 @@ export default function ReoperationPage() {
           <br />더 활짝 웃으시는모습을 위해 자만하지 않고 항상 노력하겠습니다.
         </>
       ),
+      descriptionMobile: (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ marginBottom: "2rem" }}>
+            재수술과 흉터 이식은 매일 할 수 있는
+            <br />
+            수술이 아닙니다
+            <br />
+            그만큼 경험이 쌓이는 데 오래 걸리죠.
+          </div>
+          <div style={{ marginBottom: "2rem" }}>
+            지금의 모습보다 반드시 나아질 수 있다는
+            <br />
+            자신감으로 재수술과 흉터 수술을
+            <br />
+            해왔습니다.
+          </div>
+          <div style={{ marginBottom: "2rem" }}>
+            가리고 싶은 모습 때문에 오랜 시간
+            <br />
+            힘들어했던 분들이 좋아해 주시는 모습을
+            <br />
+            보면 밥을 안 먹어도 배부릅니다.
+          </div>
+          <div>
+            더 활짝 웃으시는 모습을 위해 자만하지 않고
+            <br />
+            항상 노력하겠습니다.
+          </div>
+        </div>
+      ),
       images: {
         main: "/hair-transplant/reoperation/reoperation-4.png",
       },
